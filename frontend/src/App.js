291c60f0a204567cacb1417cc55323b32b8bfca9
Brian Buchalter <bal711@gmail.com>
2017-12-13 06:42:40 -0700

    Setup material-ui
    Adds the most basic Material UI integration so we can start styling the
    page from the beginning.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  width: '80%',
+  margin: '0 auto',
+}
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Paper style={style}>
         <h1>Readable</h1>
-      </div>
+      </Paper>
     );
   }
 }
