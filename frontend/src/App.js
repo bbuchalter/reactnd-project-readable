5a03af2cbcfef5805484090ed1b96bec6b4f3a94
Brian Buchalter <bal711@gmail.com>
2017-12-14 11:33:49 -0700

    Add navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,51 @@ import './App.css';
 import Paper from 'material-ui/Paper';
 import Posts from './Posts/index.js';
 import PostDetail from './Posts/show.js';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
-
-const style = {
-  width: '80%',
-  margin: '0 auto',
-}
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
+
+  toggleDrawer = () => this.setState({ open: !this.state.open })
+
+  appName = "Readable"
+
   render() {
     return (
-      <Paper style={style}>
+      <div>
         <AppBar
-          title="Readable"
+        title={this.appName}
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+        onLeftIconButtonClick={this.toggleDrawer}
+      />
+
+      <Drawer
+        docked={false}
+        width={300}
+        onRequestChange={this.toggleDrawer}
+        open={this.state.open}
+      >
+        <AppBar title={this.appName} onLeftIconButtonClick={this.toggleDrawer} />
+        <MenuItem
+          primaryText="All Posts"
+          containerElement={<Link to="/" />}
+          onClick={() => this.toggleDrawer()}
         />
+      </Drawer>
+
+      <Paper>
         <Route exact path="/" component={Posts} />
         <Route path="/:category/:postId" component={PostDetail} />
       </Paper>
+    </div>
     );
   }
 }
