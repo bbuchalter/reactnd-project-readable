05ae38de7cfc318e458a191000423ac3fb01d196
Brian Buchalter <bal711@gmail.com>
2017-12-14 21:57:43 -0700

    Add Api.commentVote()

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -64,6 +64,24 @@ class Api {
       .then(result => result.json())
     )
   }
+
+  static commentVote(commentId, vote) {
+    const headers = {
+      'Authorization': Api.token(),
+      'Content-Type': 'application/json',
+    }
+
+    const url = `http://localhost:3001/comments/${commentId}`
+    const options = {
+      headers,
+      method: "POST",
+      body: JSON.stringify({ option: vote })
+    }
+
+    return(fetch(url, options)
+      .then(result => result.json())
+    )
+  }
 }
 
 export default Api;
