01d21f67f039f1280eb3880d96905f737a252c66
Brian Buchalter <bal711@gmail.com>
2017-12-17 19:14:32 -0700

    Set form state when editing comments
    Because the data for the form loads async, we need to add some extra
    lifecycle hooks to ensure the data is set as state both when the props
    change or when the component is created.

--- frontend/src/Comments/edit.js
+++ frontend/src/Comments/edit.js
@@ -3,17 +3,30 @@ import { editComment } from './actions';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import { requestComments } from './actions';
 
 class EditCommentPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      body: '',
-      author: '',
+      body: this.props.body || '',
+      author: this.props.author || '',
     }
   }
 
-  handleSubmit(event) {
+  componentDidMount() {
+    const { match } = this.props;
+    this.props.requestComments(match.params.postId)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      body: nextProps.body,
+      author: nextProps.author
+    })
+  }
+
+  handleSubmit() {
     const { postId, category, commentId } = this.props.match.params;
     this.props.editComment({
       ...this.state,
@@ -25,20 +38,26 @@ class EditCommentPage extends Component {
   render() {
     return (
       <CommentForm
-        handleSubmit={(e) => this.handleSubmit(e)}
-        onAuthorChange={(e) => this.setState({author: e.target.value})}
-        onBodyChange={(e) => this.setState({body: e.target.value})}
+        handleSubmit={(e) => this.handleSubmit()}
         author={this.state.author}
         body={this.state.body}
+        onAuthorChange={(e) => this.setState({author: e.target.value})}
+        onBodyChange={(e) => this.setState({body: e.target.value})}
       />
     )
   }
 }
 
+function mapStateToProps({ comments }, ownProps) {
+  const { commentId } = ownProps.match.params;
+  return { ...comments[commentId] }
+}
+
 function mapDispatchToProps (dispatch) {
   return {
     editComment: (data) => dispatch(editComment(data)),
+    requestComments: (postId) => dispatch(requestComments(postId)),
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(EditCommentPage));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(EditCommentPage));
